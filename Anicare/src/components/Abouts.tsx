5c9f3416d164912b9eab8f1be3b6a0373f31fd97
Md Rizabul <mustafamustafa9891@gmail.com>
2024-04-13 05:03:55 +0530

    committed

---
+++ Anicare/src/components/Abouts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { About } from "./AboutVetic"
+import { GlobalStyle } from '../styles/globals'
+function Abouts() {
+  return (
+    <div>
+   
+      <GlobalStyle/>
+      <About />
+    </div>
+  )
+}
+
+export default Abouts
---
+++ Anicare/src/components/BooksAppointment.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GlobalStyle } from '../styles/globals'
+
+function BooksAppointment() {
+  return (
+    <div>
+      <GlobalStyle/>
+      Book Appoint here
+    </div>
+  )
+}
+
+export default BooksAppointment
